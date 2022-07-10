545cbacaee1b9da3cf276107e201518c1d27fb53
Bowen <root@bowen.cool>
2022-07-10 11:12:39 +0800

    update metainfo

--- dida365.js
+++ dida365.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         滴答清单自动深色模式
-// @namespace    https://dida365.com/
-// @version      0.3.3
+// @version      0.3.4
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
-// @author       https://github.com/bowencool
+// @namespace    https://dida365.com/
 // @match        https://dida365.com/webapp/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dida365.com
+// @author       https://github.com/bowencool
+// @origin       https://github.com/bowencool/Tampermonkey-Scripts/blob/main/dida365.js
+// @homepage     https://greasyfork.org/zh-CN/scripts/447649-%E6%BB%B4%E7%AD%94%E6%B8%85%E5%8D%95%E8%87%AA%E5%8A%A8%E6%B7%B1%E8%89%B2%E6%A8%A1%E5%BC%8F
 // @grant        none
 // ==/UserScript==
 
