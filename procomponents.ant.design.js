674ad5f56c16d5a14f4dcad8ed70c8817baead56
Bowen <root@bowen.cool>
2022-07-22 14:06:18 +0800

    fix

--- procomponents.ant.design.js
+++ procomponents.ant.design.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         ProComponents 官方文档自动深色模式
-// @version      0.1.0
+// @version      0.1.1
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://procomponents.ant.design/
-// @match        https://procomponents.ant.design/components/
+// @match        https://procomponents.ant.design/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=procomponents.ant.design
 // @author       bowencool
 // @license      MIT
