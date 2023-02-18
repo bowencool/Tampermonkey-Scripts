aaf47485f28c53812326f3689b3db42f46f9a77a
Bowen <root@bowen.cool>
2023-02-18 13:45:25 +0800

    fix: youtube

--- youtube.js
+++ youtube.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         YouTube自动深色模式
-// @version      0.4.1
+// @version      0.4.2
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://youtube.com/
-// @match        https://www.youtube.com/
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @author       bowencool
 // @license      MIT
