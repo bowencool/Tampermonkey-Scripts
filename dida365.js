82236c8a857ac6375b51c4eec1eff49a9daa31cd
Bowen <root@bowen.cool>
2023-08-12 12:18:35 +0800

    fix

--- dida365.js
+++ dida365.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         滴答清单自动深色模式
-// @version      0.3.11
+// @version      0.3.12
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式，使用前先在页面上把主题设置成夜间
 // @namespace    https://dida365.com/
-// @match        https://dida365.com/webapp
+// @match        https://dida365.com/webapp*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dida365.com
 // @author       bowencool
 // @license      MIT
