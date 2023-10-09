3c847db56372428ad464acc1bdb9e0d2d9e94652
Bowen <root@bowen.cool>
2023-10-09 17:06:12 +0800

    update

--- weread-auto-dark.js
+++ weread-auto-dark.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         微信读书自动深色模式
-// @version      0.1.1
+// @version      0.1.2
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://weread.qq.com/
 // @match        https://weread.qq.com/*
@@ -43,25 +43,25 @@
     console.log({ isDarkMode, currentThemeIsWhite });
     if (isDarkMode) {
       if (currentThemeIsWhite) {
-        if (location.pathname.startsWith("/web/reader")) {
-          const button = await waitForElementToExist(
-            ".readerControls_item.dark"
-          );
-          button.click();
-        } else {
-          document.body.classList.remove("wr_whiteTheme");
-        }
+        // if (location.pathname.startsWith("/web/reader")) {
+        //   const button = await waitForElementToExist(
+        //     ".readerControls_item.dark"
+        //   );
+        //   button.click();
+        // } else {
+        document.body.classList.remove("wr_whiteTheme");
+        // }
       }
     } else {
       if (!currentThemeIsWhite) {
-        if (location.pathname.startsWith("/web/reader")) {
-          const button = await waitForElementToExist(
-            ".readerControls_item.white"
-          );
-          button.click();
-        } else {
-          document.body.classList.add("wr_whiteTheme");
-        }
+        // if (location.pathname.startsWith("/web/reader")) {
+        //   const button = await waitForElementToExist(
+        //     ".readerControls_item.white"
+        //   );
+        //   button.click();
+        // } else {
+        document.body.classList.add("wr_whiteTheme");
+        // }
       }
     }
   }
