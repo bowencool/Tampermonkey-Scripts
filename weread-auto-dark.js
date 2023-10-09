9ba167c04fe2568ae64385c583c899f0fe8de2be
Bowen <root@bowen.cool>
2023-10-09 16:48:16 +0800

    update

--- weread-auto-dark.js
+++ weread-auto-dark.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         微信读书自动深色模式
-// @version      0.1.0
+// @version      0.1.1
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://weread.qq.com/
 // @match        https://weread.qq.com/*
@@ -38,16 +38,30 @@
   async function toggle(
     isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
   ) {
-
-    const currentThemeIsWhite = document.body.classList.contains("wr_whiteTheme");
+    const currentThemeIsWhite =
+      document.body.classList.contains("wr_whiteTheme");
     console.log({ isDarkMode, currentThemeIsWhite });
     if (isDarkMode) {
       if (currentThemeIsWhite) {
-        document.body.classList.remove("wr_whiteTheme");
+        if (location.pathname.startsWith("/web/reader")) {
+          const button = await waitForElementToExist(
+            ".readerControls_item.dark"
+          );
+          button.click();
+        } else {
+          document.body.classList.remove("wr_whiteTheme");
+        }
       }
     } else {
       if (!currentThemeIsWhite) {
-        document.body.classList.add("wr_whiteTheme");
+        if (location.pathname.startsWith("/web/reader")) {
+          const button = await waitForElementToExist(
+            ".readerControls_item.white"
+          );
+          button.click();
+        } else {
+          document.body.classList.add("wr_whiteTheme");
+        }
       }
     }
   }
