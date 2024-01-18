134d00f1d13980e6c2ca50d2b0eec17e1df8da04
Bowen <root@bowen.cool>
2024-01-18 16:43:07 +0800

    update icon

--- ant-design-auto-dark.js
+++ ant-design-auto-dark.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         antd 官方文档自动深色模式
-// @version      2.0.0
+// @version      2.0.1
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://ant.design/
 // @match        https://ant.design/*
 // @match        https://ant-design.antgroup.com/*
 // @match        https://ant-design.gitee.io/*
-// @icon         https://gw.alipayobjects.com/zos/rmsportal/rlpTLlbMzTNYuZGGCVYM.png
+// @icon         https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg
 // @author       bowencool
 // @license      MIT
 // @homepageURL  https://greasyfork.org/scripts/447698
--- antd-redirection.js
+++ antd-redirection.js
@@ -2,10 +2,10 @@
 // @name         antd redirection
 // @description  重定向到国内镜像
 // @namespace    antd
-// @version      1.0.1
+// @version      1.0.2
 // @author       bowencool
 // @match        https://ant.design/*
-// @icon         https://gw.alipayobjects.com/zos/rmsportal/rlpTLlbMzTNYuZGGCVYM.png
+// @icon         https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg
 // @grant        none
 // @license      MIT
 // @homepageURL  https://greasyfork.org/scripts/485132
