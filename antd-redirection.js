f35a2df8f84d201d34c9835c4e8962a3da9ff469
Bowen <root@bowen.cool>
2024-01-18 16:19:48 +0800

    update

--- antd-redirection.js
+++ antd-redirection.js
@@ -2,13 +2,13 @@
 // @name         antd redirection
 // @description  重定向到国内镜像
 // @namespace    antd
-// @version      0.1.0
+// @version      1.0.0
 // @author       bowencool
 // @match        https://ant.design/*
 // @icon         https://ant-design.antgroup.com/favicon.ico
 // @grant        none
 // @license      MIT
-// @homepageURL  https://greasyfork.org/scripts/447649
+// @homepageURL  https://greasyfork.org/scripts/485132
 // @supportURL   https://github.com/bowencool/Tampermonkey-Scripts/issues
 // @run-at       document-start
 // ==/UserScript==
