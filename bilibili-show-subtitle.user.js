f542f53e8fd1abedf5e0e1b370a5888014cfb873
Bowen <root@bowen.cool>
2024-02-01 12:42:00 +0800

    feat: auto refresh subtitles when url changed

--- bilibili-show-subtitle.user.js
+++ bilibili-show-subtitle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         在侧边显示 Bilibili 视频字幕/文稿
 // @name:en      Show transcript of Bilibili video on the side
-// @version      1.0.4
+// @version      1.1.0
 // @description:en  Automatically display Bilibili video subtitles/scripts by default, support click to jump, text selection, auto-scrolling.
 // @description     默认自动显示Bilibili视频字幕/文稿，支持点击跳转、文本选中、自动滚动。
 // @namespace    https://bilibili.com/
@@ -100,16 +100,58 @@ function parseTime(t) {
   return `${fixNumber(parseInt(t / 60))}:${fixNumber(t % 60)}`;
 }
 
-(async function () {
-  "use strict";
+const transcriptBox = document.createElement("div");
+transcriptBox.className = "transcript-box";
+async function showTranscript(subtitleInfo) {
+  console.log("showTranscript", subtitleInfo);
+  const { body: lines } = await fetch(
+    subtitleInfo.subtitle_url.replace(/^\/\//, "https://")
+  ).then((res) => res.json());
+  console.log("lines", lines);
+  transcriptBox.innerHTML = "";
+  for (let line of lines) {
+    if (line.music && line.music > MUSIC_FILTER_RATE) {
+      continue;
+    }
+    let timeLink = document.createElement("a");
+    timeLink.className = "transcript-line-time";
+    // timeLink.setAttribute("data-index", line.index);
+    timeLink.textContent = parseTime(line.from);
+    timeLink.addEventListener("click", () => {
+      video.currentTime = line.from;
+    });
+    let lineDiv = document.createElement("div");
+    lineDiv.className = "transcript-line";
+    lineDiv.setAttribute("data-from", line.from);
+    lineDiv.setAttribute("data-to", line.to);
+    lineDiv.appendChild(timeLink);
+    let span = document.createElement("span");
+    span.className = "transcript-line-content";
+    span.textContent = line.content;
 
-  const bvid = window.location.pathname.match(/\/video\/(\w+)/)?.[1];
-  if (!bvid) return;
+    lineDiv.appendChild(span);
+    transcriptBox.appendChild(lineDiv);
+  }
+}
 
-  let curPage = new URLSearchParams(window.location.search).get("p") - 1;
+function getBvid(route /* : string|undefined */) {
+  let url;
+  if (route) {
+    url = new URL(window.location.origin + route);
+  } else {
+    url = new URL(window.location.href);
+  }
+  const bvid = url.pathname.match(/\/video\/(\w+)/)?.[1];
+  // if (!bvid) throw new Error("没有找到 bvid");
+  let curPage = url.searchParams.get("p") - 1;
   if (!curPage || curPage == -1) {
     curPage = 0;
   }
+  return { bvid, curPage };
+}
+async function getTranscript(route /* : string|undefined */) {
+  const { bvid, curPage } = getBvid(route);
+  if (!bvid) throw new Error("没有找到 bvid");
   const videoInfo = await request("/x/web-interface/view?bvid=" + bvid);
   const {
     subtitle: { subtitles = [] },
@@ -117,13 +159,17 @@ function parseTime(t) {
     `/x/player/v2?aid=${videoInfo.aid}&cid=${videoInfo.pages[curPage].cid}`
   );
   console.log("subtitles", subtitles);
-  if (subtitles.length == 0) return console.log("没有字幕");
+  if (subtitles.length == 0) throw new Error("没有字幕");
+  return subtitles;
+}
+
+async function main() {
+  "use strict";
+  const subtitles = await getTranscript();
 
   // B站页面是SSR的，如果插入过早，页面 js 检测到实际 Dom 和期望 Dom 不一致，会导致重新渲染
-  await waitForElementToExist("img.bili-avatar-img")
+  await waitForElementToExist("img.bili-avatar-img");
   const video = await waitForElementToExist("video");
-  const transcriptBox = document.createElement("div");
-  transcriptBox.className = "transcript-box";
   // const oldfanfollowEntry = await waitForElementToExist("#oldfanfollowEntry");
   video.addEventListener("timeupdate", () => {
     const currentTime = video.currentTime;
@@ -157,35 +203,69 @@ function parseTime(t) {
   const danmukuBox = await waitForElementToExist("#danmukuBox");
   // B站页面是SSR的，如果插入过早，页面 js 检测到实际 Dom 和期望 Dom 不一致，会导致重新渲染
   danmukuBox.parentNode.insertBefore(transcriptBox, danmukuBox);
+}
 
-  async function showTranscript(subtitleInfo) {
-    console.log("showTranscript", subtitleInfo);
-    const { body: lines } = await fetch(
-      subtitleInfo.subtitle_url.replace(/^\/\//, "https://")
-    ).then((res) => res.json());
-    console.log("lines", lines);
-    for (let line of lines) {
-      if (line.music && line.music > MUSIC_FILTER_RATE) {
-        continue;
-      }
-      let timeLink = document.createElement("a");
-      timeLink.className = "transcript-line-time";
-      // timeLink.setAttribute("data-index", line.index);
-      timeLink.textContent = parseTime(line.from);
-      timeLink.addEventListener("click", () => {
-        video.currentTime = line.from;
-      });
-      let lineDiv = document.createElement("div");
-      lineDiv.className = "transcript-line";
-      lineDiv.setAttribute("data-from", line.from);
-      lineDiv.setAttribute("data-to", line.to);
-      lineDiv.appendChild(timeLink);
-      let span = document.createElement("span");
-      span.className = "transcript-line-content";
-      span.textContent = line.content;
+async function updateTranscript(route /* : string|undefined */) {
+  const subtitles = await getTranscript(route);
+  await showTranscript(subtitles[0]);
+}
 
-      lineDiv.appendChild(span);
-      transcriptBox.appendChild(lineDiv);
+main();
+
+function getCurrentState(route) {
+  const { bvid, curPage } = getBvid(route);
+  return `${bvid}?p=${curPage}`;
+}
+let lastState = getCurrentState();
+traceRoute();
+
+function traceRoute() {
+  // popstate 可以监测到 hashchange
+  window.addEventListener("popstate", (evt) => {
+    const to = getCurrentState();
+    if (to !== lastState) {
+      console.log("bvid changed when popstate", lastState, to);
+      updateTranscript();
     }
+  });
+  let theHistory /* History */ = history || window.history;
+  if (!theHistory) return;
+
+  const replacement = (originFn /* History['pushState'] */) => {
+    return (data /* any */, t /* string */, route /* string | undefined */) => {
+      const from = getCurrentState();
+      const to = getCurrentState(route);
+      if (route && from !== to) {
+        console.log("bvid changed when pushState", from, to, route);
+        updateTranscript(route);
+      }
+      const ret = originFn.call(theHistory, data, t, route);
+      if (to) {
+        lastState = to;
+      }
+      return ret;
+    };
+  };
+  overrideMethod(
+    /* <History['pushState']> */ theHistory,
+    "pushState",
+    replacement
+  );
+  overrideMethod(
+    /* <History['replaceState']> */ theHistory,
+    "replaceState",
+    replacement
+  );
+}
+function overrideMethod /* <F extends Function> */(
+  target /* : { [key: string]: any } */,
+  key /* : string */,
+  replacement /* : (f: F) => F */
+) {
+  if (!(key in target)) return;
+  const originFn /* : F */ = target[key];
+  const wrapped /* : F */ = replacement(originFn);
+  if (wrapped instanceof Function) {
+    target[key] = wrapped;
   }
-})();
+}
