7477e4c439255df3e5b4bfda7b9d43c9cf77a69b
Bowen <root@bowen.cool>
2024-04-01 15:57:25 +0800

    fix time jumping

--- bilibili-show-subtitle.user.js
+++ bilibili-show-subtitle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         在侧边显示 Bilibili 视频字幕/文稿
 // @name:en      Show transcript of Bilibili video on the side
-// @version      1.1.1
+// @version      1.1.2
 // @description:en  Automatically display Bilibili video subtitles/scripts by default, support click to jump, text selection, auto-scrolling.
 // @description     默认自动显示Bilibili视频字幕/文稿，支持点击跳转、文本选中、自动滚动。
 // @namespace    https://bilibili.com/
