35790e239314d9d8bc0e08fe9ab26673c78917de
Bowen <root@bowen.cool>
2024-09-17 14:48:51 +0800

    update

--- ibkr-auto-dark.js
+++ ibkr-auto-dark.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         IBKR盈透证券自动深色模式
-// @version      1.0.0
+// @version      1.0.1
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://www.ibkr.com.cn/
 // @match        https://www.ibkr.com.cn/portal*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ibkr.com
 // @author       bowencool
 // @license      MIT
-// @homepageURL  https://greasyfork.org/scripts/000000
+// @homepageURL  https://greasyfork.org/scripts/508799
 // @supportURL   https://github.com/bowencool/Tampermonkey-Scripts/issues
 // @require      https://cdn.jsdelivr.net/gh/bowencool/Tampermonkey-Scripts@f59cc91442dd34eb28e0d270486da5c7ac8d2d50/shared/cookie.js
 // @require      https://cdn.jsdelivr.net/gh/bowencool/Tampermonkey-Scripts@f59cc91442dd34eb28e0d270486da5c7ac8d2d50/shared/waitForElementToExist.js
