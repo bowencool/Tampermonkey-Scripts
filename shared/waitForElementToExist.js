b65b677146fdf0d0af884371a943d7f4a65f6ec8
Bowen <root@bowen.cool>
2024-11-05 20:44:41 +0800

    add time out

--- shared/waitForElementToExist.js
+++ shared/waitForElementToExist.js
@@ -1,15 +1,15 @@
-function waitForElementToExist(selector) {
-  return new Promise((resolve) => {
+function waitForElementToExist(selector, timeout) {
+  return new Promise((resolve, reject) => {
     if (document.querySelector(selector)) {
-      console.log("resolved", selector, document.querySelector(selector));
+      // console.log("resolved", selector, document.querySelector(selector));
       return resolve(document.querySelector(selector));
     }
-    console.log("wait for", selector);
+    // console.log("wait for", selector);
     const observer = new MutationObserver(() => {
       const el = document.querySelector(selector);
       if (el) {
         resolve(el);
-        console.log("resolved", selector, el);
+        // console.log("resolved", selector, el);
         observer.disconnect();
       }
     });
@@ -18,5 +18,11 @@ function waitForElementToExist(selector) {
       subtree: true,
       childList: true,
     });
+    if (timeout > 0) {
+      setTimeout(() => {
+        observer.disconnect();
+        reject(new Error("Timeout"));
+      }, timeout);
+    }
   });
 }
