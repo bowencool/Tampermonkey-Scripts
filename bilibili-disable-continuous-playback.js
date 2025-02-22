10652660a5d20a7cc9a020143379891cf9e6515d
Bowen <root@bowen.cool>
2025-02-22 10:46:23 +0800

    update

--- bilibili-disable-continuous-playback.js
+++ bilibili-disable-continuous-playback.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         禁止 Bilibili 连播
-// @name:en      Prohibit continuous playback for Bilibili
-// @version      0.1.0
+// @name:en      Disable continuous playback for Bilibili
+// @version      1.0.0
 // @description:en  Disable continuous playback for Bilibili video.
 // @description     禁止 Bilibili 视频的连续播放。
 // @namespace    https://bilibili.com/
@@ -9,7 +9,7 @@
 // @icon         https://www.bilibili.com/favicon.ico
 // @author       bowencool
 // @license      MIT
-// @homepageURL  https://greasyfork.org/scripts/482165
+// @homepageURL  https://greasyfork.org/scripts/527708
 // @supportURL   https://github.com/bowencool/Tampermonkey-Scripts/issues
 // @require      https://cdn.jsdelivr.net/gh/bowencool/Tampermonkey-Scripts@b65b677146fdf0d0af884371a943d7f4a65f6ec8/shared/waitForElementToExist.js
 // ==/UserScript==
