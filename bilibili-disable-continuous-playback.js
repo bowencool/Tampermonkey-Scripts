ace1cb0fe0b1fc8bc702de3b470dacb819fee7bd
Bowen <root@bowen.cool>
2025-02-22 10:59:36 +0800

    fix

--- bilibili-disable-continuous-playback.js
+++ bilibili-disable-continuous-playback.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         禁止 Bilibili 连播
 // @name:en      Disable continuous playback for Bilibili
-// @version      1.0.2
+// @version      1.0.3
 // @description:en  Disable continuous playback for Bilibili video.
 // @description     禁止 Bilibili 视频的连续播放。
 // @namespace    https://bilibili.com/
@@ -14,7 +14,9 @@
 // @require      https://cdn.jsdelivr.net/gh/bowencool/Tampermonkey-Scripts@b65b677146fdf0d0af884371a943d7f4a65f6ec8/shared/waitForElementToExist.js
 // ==/UserScript==
 
-// B站页面是SSR的，这里需要等待客户端接管完成
-await waitForElementToExist("img.bili-avatar-img");
-const el = await waitForElementToExist(".continuous-btn .switch-btn.on");
-el.click();
+(async function () {
+  // B站页面是SSR的，这里需要等待客户端接管完成
+  await waitForElementToExist("img.bili-avatar-img");
+  const el = await waitForElementToExist(".continuous-btn .switch-btn.on");
+  el.click();
+})();
