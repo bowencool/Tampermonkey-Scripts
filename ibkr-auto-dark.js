98ff30da11e70fcfbd363b0df9f38cf876adb7fb
Bowen <root@bowen.cool>
2026-08-31 23:12:45 +0800

    update ibkr-auto-dark.js: expand match rules and refine selector

--- ibkr-auto-dark.js
+++ ibkr-auto-dark.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         IBKR盈透证券自动深色模式
-// @version      1.0.2
+// @version      1.0.3
 // @description  根据系统设置自动切换深色模式，深色用的是官方的样式
 // @namespace    https://www.ibkr.com.cn/
-// @match        https://www.ibkr.com.cn/portal*
+// @match        https://www.ibkr.com.cn/*
+// @match        https://ndcdyn.interactivebrokers.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ibkr.com.cn
 // @author       bowencool
 // @license      MIT
@@ -24,13 +25,13 @@
     console.log({ isDarkMode, currentTheme });
     if (isDarkMode) {
       if (currentTheme === "dark") return;
-      (await waitForElementToExist(".one-head-menu button")).click();
+      (await waitForElementToExist(".one-head-menu>button:last-of-type")).click();
       Array.from(document.querySelectorAll("button.one-user-btn"))
         .find((el) => ["Dark", "深色"].includes(el.textContent))
         ?.click();
     } else {
       if (currentTheme === "light") return;
-      (await waitForElementToExist(".one-head-menu button")).click();
+      (await waitForElementToExist(".one-head-menu>button:last-of-type")).click();
       Array.from(document.querySelectorAll("button.one-user-btn"))
         .find((el) => ["Light", "浅色"].includes(el.textContent))
         ?.click();
